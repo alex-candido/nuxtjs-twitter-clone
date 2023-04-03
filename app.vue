5e06d6dce87e28097fcb5d31ea90fcbf7878377d
alex-candido <alex.candido.tec@gmail.com>
2023-04-02 22:06:11 -0300

    estrutura inicial layout de app

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+let darkMode = ref(true) as boolean;
+
+</script>
+
+<template>
+  <div :class="{'dark': darkMode}">
+    <div class="min-h-full ">
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+
+      </div>
+    </div>
+  </div>
+</template>
