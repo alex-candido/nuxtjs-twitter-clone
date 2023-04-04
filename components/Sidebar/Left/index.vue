e5382e9d893e0f55356eabba54e94ba2667acefc
Alex Cândido <alex.candido.tec@gmail.com>
2023-04-04 17:56:47 -0300

    estrutura inicial component left

---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { defaultTransition } = useTailwindConfig();
+
+</script>
+
+<template>
+  <div class="flex flex-col h-screen ">
+    <div
+      class="p-2 my-2 hover:bg-blue-50 rounded-full dark:hover:bg-white/20 w-min" 
+      :class="defaultTransition"
+    >
+      <nuxt-link to="/">
+        <div class="w-9 h-9">
+          <font-awesome-icon class="text-4xl text-sky-500" icon="fa-brands fa-twitter" />
+        </div>
+      </nuxt-link>
+    </div>
+    <font-awesome-icon class="text-2xl text-white" icon="fa-solid fa-house" />
+
+  </div>
+</template>
+
