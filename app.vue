f4d9105a7cd8061af934ddf95db3fcb024c6b47c
alex-candido <alex.candido.tec@gmail.com>
2023-04-05 00:42:45 -0300

    estrutura inicial component Tab

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@ let darkMode = ref(true);
 
 <template>
   <div :class="{'dark': darkMode}">
-    <div class="bg-white dark:bg-dim-900">
+    <div class="bg-white dark:bg-black">
       <div class="min-h-full ">
       <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
         
