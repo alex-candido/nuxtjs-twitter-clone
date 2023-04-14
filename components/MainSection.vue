f07de79e754256c67227667108081741bab1ec64
Alex Cândido <alex.candido.tec@gmail.com>
2023-04-14 15:52:03 -0300

    nova estrutura de app e pages

---
+++ components/MainSection.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    const props = defineProps({
+        title: {
+            type: String,
+            required: true
+        },
+        loading: {
+            type: Boolean,
+            required: true
+        }        
+    })
+
+</script>
+
+<template>
+    <div>
+
+        <h2>{{ props.title }}</h2>
+
+        <slot></slot>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,12 @@
-<script lang="ts" setup>
-let darkMode = ref(true);
+<script setup lang="ts">
+
 
 </script>
 
 <template>
-  <div :class="{'dark': darkMode}">
-    <div class="bg-white dark:bg-black">
-      <div class="min-h-full ">
-      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-10">
-        
-        <!-- Left Sidebar -->
-
-        <div class="md:block xs-col-span-1 xl:col-span-2">
-          <div class="sticky top-0">
-            <SidebarLeft />
-          </div>
-        </div>
-
-        <!-- Main Content -->
-
-        <main class="col-span-12 bg-sky-500 md:col-span-8 xl:col-span-6">
-          <h1>main</h1>
-        </main>
-
-        <!-- Right Sidebar -->
-
-        <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
-          <div class="sticky top-0">
-            <SidebarRight />
-          </div>
-        </div>
-
-      </div>
+    <div>
+        <MainSection title="Home" :loading="false">
+            Content
+        </MainSection>
     </div>
-    </div>
-  </div>
 </template>
