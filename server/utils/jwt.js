5bc5e7aee0c41ad0bf59f6634c403f042287333d
Alex Cândido <alex.candido.tec@gmail.com>
2023-05-09 16:59:46 -0300

    adicionado estrutura de generateAccessToken, generateRefreshToken em server/utils/jwt.js

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,11 +1,19 @@
 import jwt from "jsonwebtoken"
 
 const generateAccessToken = (user) => {
+    const config = useRuntimeConfig()
 
+    return jwt.sign({ userId: user.id }, config.jwtAccessSecret, {
+        expiresIn: '10m'
+    })
 }
 
 const generateRefreshToken = (user) => {
-    
+    const config = useRuntimeConfig()
+
+    return jwt.sign({ userId: user.id }, config.jwtRefreshSecret, {
+        expiresIn: '4h'
+    })
 }
 
 export const generateTokens = (user) => {
