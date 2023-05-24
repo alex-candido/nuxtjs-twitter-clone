b38d2bd90533414c2937ba9dfd0e71deb01597b3
Alex Cândido <alex.candido.tec@gmail.com>
2023-05-24 19:00:34 -0300

    adicionado estrutura createRefreshToken, getRefreshTokenByToken, removeRefreshToken em server/db/refreshTokens.ts

--- server/db/refreshTokens.ts
+++ server/db/refreshTokens.ts
@@ -1,13 +1,28 @@
-// import { prisma } from "."
+import { prisma } from ".";
 
-export const createRefreshToken = (refreshToken: any) => {
+interface refreshTokenProps {
+  token: string;
+  userId: string;
+}
 
+export const createRefreshToken = (refreshToken: refreshTokenProps) => {
+  return prisma.refreshToken.create({
+    data: refreshToken,
+  })
 }
 
 export const getRefreshTokenByToken = (token: any) => {
-
+  return prisma.refreshToken.findUnique({
+    where: {
+      token,
+    }
+  })
 }
 
 export const removeRefreshToken = (token: any) => {
-
+  return prisma.refreshToken.delete({
+    where: {
+      token: token,
+    }
+  })
 }
