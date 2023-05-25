f6c47c3e0b20a7667d02e35449b2d09d0011b248
Alex Cândido <alex.candido.tec@gmail.com>
2023-05-24 21:52:18 -0300

    adicionado estrutura inicial Form em components/Auth/Form.vue

--- app.vue
+++ app.vue
@@ -1,37 +1,38 @@
 <script lang="ts" setup>
 let darkMode = ref(false);
-
 </script>
 
 <template>
-  <div :class="{'dark': darkMode}">
+  <div :class="{ dark: darkMode }">
     <div class="bg-white dark:bg-black">
-      <div class="min-h-full ">
-      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-10">
-        
-        <!-- Left Sidebar -->
+      <!-- App -->
+      <div v-if="false" class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-10"
+        >
+          <!-- Left Sidebar -->
 
-        <div class="md:block xs-col-span-1 xl:col-span-2">
-          <div class="sticky top-0">
-            <SidebarLeft />
+          <div class="md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+          <!-- Main Content -->
+
+          <main class="col-span-12 marker:md:col-span-8 xl:col-span-6">
+            <router-view />
+          </main>
+
+          <!-- Right Sidebar -->
+
+          <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
+            <div class="sticky top-0">
+              <SidebarRight />
+            </div>
           </div>
         </div>
-        <!-- Main Content -->
-
-        <main class="col-span-12 marker:md:col-span-8 xl:col-span-6">
-          <router-view/>
-        </main>
-
-        <!-- Right Sidebar -->
-
-        <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
-          <div class="sticky top-0">
-            <SidebarRight />
-          </div>
-        </div>
-
       </div>
-    </div>
+      <AuthPage v-else />
     </div>
   </div>
 </template>
---
+++ components/Auth/Form.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full">
+    <div class="flex justify-center">
+      <div class="w-10 h-10">
+        <font-awesome-icon
+          class="text-4xl text-sky-500"
+          icon="fa-brands fa-twitter"
+        />
+      </div>
+    </div>
+
+    <div class="pt-5 space-y-6">Login</div>
+  </div>
+</template>
