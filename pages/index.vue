2dc1b45ff8118fdb16fa860a7e121db935876fde
Alex Cândido <alex.candido.tec@gmail.com>
2023-05-28 22:31:38 -0300

    adicionado estrutura reRefreshAccessToken em composables/useAuth.ts

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
     const loading = ref(false)
+    const { useAuthUser } = useAuth()
+    const user = useAuthUser()
 
 </script>
 
 <template>
     <div>
-        <MainSection title="Home" :loading="true">
-            this is our home page
+        <MainSection title="Home" :loading="loading">
+          <Head>
+              <Title>Home / Twitter</Title>
+          </Head>
+
         </MainSection>
     </div>
 </template>
